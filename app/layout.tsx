bf485968f94a9d21d3c95789412b2d0d4bba7f19
laodefarhan <tilaodefarhanfdllh@gmail.com>
2025-05-21 18:47:55 +0700

    Update layout and page components; remove unused providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,27 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// Metadata SEO-Optimized
 export const metadata: Metadata = {
   title: "Laode F. Fadilah",
-  description: "Portfolio Website by Laode F. Fadilah",
+  description: "Portfolio Website by Laode F. Fadilah. Jelajahi keahlian saya dalam pengembangan aplikasi web dan Android.",
+  keywords:
+    "Laode Farhan Fadilah, full stack developer Indonesia, junior developer, pengembang web, portfolio pengembang aplikasi, frontend backend, aplikasi Android",
+  authors: [{ name: "Laode Farhan Fadilah", url: "https://laodefarhan.vercel.app/" }],
+  creator: "Laode Farhan Fadilah",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    type: "website",
+    locale: 'id_ID',
+    title: "Portfolio Junior Full Stack Developer",
+    description:
+      "Lihat keahlian dan pengalaman dalam membangun solusi digital end-to-end, dari frontend hingga backend, termasuk aplikasi Android.",
+    url: "https://laodefarhan.vercel.app/",
+    siteName: "Laode Farhan Fadilah Portfolio",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,6 @@ import { HeroSection } from "@/components/sections/hero";
 import { ResumeSection } from "@/components/sections/resume";
 import { ServicesSection } from "@/components/sections/services";
 
-// Metadata SEO-optimized
-export const metadata = {
-  title: "Junior Full Stack Developer | Portfolio Website",
-  description:
-    "Selamat datang di portofolio pribadi saya. Saya adalah seorang junior full stack developer. Keahlian saya dalam membangun website aplikasi dan aplikasi android",
-  keywords:
-    "full stack developer, junior developer, portfolio web, pengembang aplikasi, web development, android development, developer Indonesia",
-  authors: [{ name: "Laode Farhan Fadilah", url: "https://laodefarhan.vercel.app/" }],
-  creator: "Laode Farhan Fadilah",
-  openGraph: {
-    title: "Portfolio Junior Full Stack Developer",
-    description:
-      "Lihat keahlian dan pengalaman dalam membangun solusi digital end-to-end, dari frontend hingga backend, termasuk aplikasi Android.",
-    url: "https://laodefarhan.vercel.app/",
-    siteName: "LaodeFarhan.",
-  },
-};
-
 export default function HomePage() {
   return (
     <main className="bg-gray-900 text-white overflow-hidden">
